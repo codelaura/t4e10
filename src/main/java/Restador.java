06bc2111cf39a486286fecd3a5e596c9bf6cbedb
Laura GC <laura.garcia.cast@gmail.com>
2021-03-04 23:23:58 +0100

    Restador

---
+++ src/main/java/Restador.java
@@ -0,0 +1,4 @@
+public class Restador {
+public static float restar (float a, float b ){
+return a - b;
+}}
--- src/main/java/Sumador.java
+++
@@ -1,4 +0,0 @@
-public class Sumador {
-public static float sumar (float a, float b ) {
-return a + b;
-}}
